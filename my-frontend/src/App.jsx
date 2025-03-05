d74bb19d7127aa1a47b9e2d0552123bd4c23c8ea
Betran <beyranjr99@gmail.com>
2025-03-05 11:14:18 +0800

    Add ProdukList component with state and effect

---
+++ my-frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ProdukList from "./components/ProdukList";
+function App() {
+  return (
+    <div>
+      <h1>Selamat Datang di Aplikasi E-Commerce Sederhana</h1>
+      <ProdukList />
+    </div>
+  );
+}
+export default App;
---
+++ my-frontend/src/components/ProdukList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+function ProdukList() {
+  return (
+    <div>
+      <h2>Daftar Produk</h2>
+      <ul>
+        <li>Contoh Produk A</li>
+        <li>Contoh Produk B</li>
+      </ul>
+    </div>
+  );
+}
+export default ProdukList;
